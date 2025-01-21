a4107ed5b7b1a92d1b9c298ffd5f58a3e2fbeac4
Hei Wan Lee <hwelee0105@gmail.com>
2025-01-21 17:30:05 +0000

    update flan-UL2 code for GPU

--- flan-UL2_example.py
+++ flan-UL2_example.py
@@ -11,3 +11,14 @@ outputs = model.generate(inputs, max_length=200)
 
 print(tokenizer.decode(outputs[0]))
 # <pad> They have 23 - 20 = 3 apples left. They have 3 + 6 = 9 apples. Therefore, the answer is 9.</s>
+
+
+# with GPU
+# from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+# model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-ul2", load_in_8bit=True, device_map="auto")
+# tokenizer = AutoTokenizer.from_pretrained("google/flan-ul2")
+
+# inputs = tokenizer("A step by step recipe to make bolognese pasta:", return_tensors="pt")
+# outputs = model.generate(**inputs)
+# print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
